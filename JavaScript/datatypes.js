bef5e7fcdda75eec340f91968ce35ebda55b22c7
Nsikak Akpan <builtbykaksy@gmail.com>
2024-08-21 10:31:12 +0100

    Javascript folder update

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,43 @@
+// Datatypes and variables
+
+// string variables
+let myName = "Emmanuel Patrick";
+var myCountry = 'USA';
+const myAge =  `nine-teen`;
+
+// number variables
+let myNumber = -20;
+let myDecimalNumber = 3.14;
+
+// boolean variables
+let isTrue = true;
+let isFalse = false;
+
+// undefined variables
+let myUndefinedVariable;
+
+// null variables
+let myNullVariable = null;
+        
+// array variables
+let myFirstArray = [];
+let myArray = [1, "two", isFalse, undefined,  myName, [3, 5.6]];
+
+// bigint variables
+let myBigInt = 12345678901234567890n;
+
+// object variables
+let myFirstbObject = {};
+
+let myObject = {
+    name: 'Mfoniso',
+    age: 12,
+    isMarried: false,
+    teskbooks_owned: ['English', "Mathematics", 'Primary Science'],
+    physicalStats: {
+        height: '100cm',
+        weight: '50kg'
+    },
+    assetsOwned: [car]
+};
+
