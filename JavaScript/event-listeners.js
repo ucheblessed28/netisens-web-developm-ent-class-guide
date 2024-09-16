8adca14980e21ce72f74fc6b4c03e787d44b6f31
Nsikak Akpan <builtbykaksy@gmail.com>
2024-09-16 10:40:53 +0100

    events

--- JavaScript/event-listeners.js
+++ JavaScript/event-listeners.js
@@ -19,71 +19,85 @@
 
 
 // Common Types of Event Listeners
+const button = document.getElementById("myButton");
+
+console.log(button);
 
 // A.) Mouse Events
 
 // 1.) Click: Triggered when an element is clicked.
 
-// const button = document.getElementById("myButton");
-// console.log(button);
-
-// const handleClick = () => {
-//     alert("Button was clicked!");
-// }
+const handleClick = () => {
+    alert("Button was clicked!");
+}
 
 // button.addEventListener("click", handleClick);
 
-// button.addEventListener("click", function() {
-//   alert("Button was clicked!");
+
+// button.addEventListener("click", function(){
+//     alert("Button was clicked using anonymous callback function!");
 // });
 
+
 // 2.) dblclick: Triggered when an element is double-clicked.
-const button = document.getElementById("myButton");
-button.addEventListener("dblclick", function() {
-  alert("Button was double-clicked!");
-});
+// button.addEventListener("dblclick", function() {
+//     alert("Button was double-clicked!");
+//   });
 
 // 3.) mouseover: Triggered when the mouse pointer is moved over an element.
-// const button = document.getElementById("myButton");
+
 // button.addEventListener("mouseover", function() {
-//   button.style.backgroundColor = "blue";
+//   button.style.backgroundColor = "red";
 // });
 
 // 4.) mouseout: Triggered when the mouse pointer is moved out of an element.
-// const button = document.getElementById("myButton");
+
 // button.addEventListener("mouseout", function() {
-//   button.style.backgroundColor = "";
+//   button.style.backgroundColor = "green";
 // });
 
 // B.) Keyboard Events
 // 1.) keydown: Triggered when a key is pressed down.
 // document.addEventListener("keydown", function(event) {
+//     console.log(event);
 //     console.log("Key pressed: " + event.key);
-//   });
+//     if(event.keyCode === 13){
+//         alert('You clicked on Enter')
+//     }
+//     button.style.backgroundColor = "yellow";
+// });
 
 // 2.) keyup: Triggered when a key is released.
 // document.addEventListener("keyup", function(event) {
+//     const body = document.querySelector("body");
+//     body.style.backgroundColor = "pink";
 //     console.log("Key released: " + event.key);
 //   });
 
 // 3.) keypress: Triggered when a key is pressed (deprecated, use keydown or keyup).
 // document.addEventListener("keypress", function(event) {
+//     const body = document.querySelector("body");
+//     body.style.backgroundColor = "red";
 //     console.log("Key pressed: " + event.key);
 //   });
   
 
 // C.) Form Events
 // 1.) submit: Triggered when a form is submitted.
-// const form = document.getElementById("myForm");
+const form = document.getElementById("myForm");
 // form.addEventListener("submit", function(event) {
 //   event.preventDefault(); // Prevents the form from being submitted
 //   alert("Form submitted!");
 // });
 
 // 2.) change: Triggered when the value of an input element changes.
-// const input = document.getElementById("myInput");
-// input.addEventListener("change", function() {
-//   console.log("Input value changed: " + input.value);
+// const input = document.getElementById("name");
+// console.log(input);
+// // console.log("Value: " + input.value);
+
+// input.addEventListener("input", function(e) {
+//   console.log(e.target);  
+//   console.log("Input value changed: " + e.target.value);  
 // });
 
 // 3.) focus: Triggered when an element (like an input) gains focus.
@@ -101,19 +115,19 @@ button.addEventListener("dblclick", function() {
 
 // D.) Window Events
 // 1.) load:Triggered when the entire page has loaded.
-// window.addEventListener("load", function() {
-//     alert("Page has fully loaded!");
-//   });
+window.addEventListener("load", function() {
+    alert("Page has fully loaded!");
+  });
 
 // 2.) resize: Triggered when the browser window is resized.
-// window.addEventListener("resize", function() {
-//     console.log("Window resized to: " + window.innerWidth + "x" + window.innerHeight);
-//   });
+window.addEventListener("resize", function() {
+    console.log("Window resized to: " + window.innerWidth + "x" + window.innerHeight);
+  });
 
 // 3.) scroll: Triggered when the user scrolls the page.
-// window.addEventListener("scroll", function() {
-//     console.log("Page scrolled!");
-//   });
+window.addEventListener("scroll", function() {
+    console.log("Page scrolled!");
+  });
 
 
 
